aa2e82d3ad8984ecffb0b3b1d1c28eb221395d87
hamza <hamza.chaouche@prevision.io>
2020-04-07 16:49:20 +0200

    TEST : defined 1st lil test

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Logo from '@/components/openasso-logo.vue'
 
 describe('Logo', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
+    test('is a Vue instance', () => {
+        const wrapper = mount(Logo)
+        expect(wrapper.isVueInstance()).toBeTruthy()
+    })
 })
