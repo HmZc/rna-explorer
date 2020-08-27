97f5dbf11701ca4915881df84adeecebc25e7d55
HmzC <hamza.chaouche@gmail.com>
2020-08-27 15:45:02 +0200

    NF: can export dataset

--- components/openasso-header.vue
+++ components/openasso-header.vue
@@ -20,10 +20,16 @@
                 nuxt-link(to="/") Rechercher
             a-menu-item(key="explorer" disabled)                                                                                           
                 nuxt-link(to="/explorer") Explorer
+        a-button(class="export-data-btn" type="danger" ghost icon="download" href="https://public.opendatasoft.com/explore/dataset/associations/download/?format=csv&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B") Exporter le jeu de données (csv)
 </template>
 
 <style lang="scss" scoped>
     .ant-layout-header {
+        display: flex;
+        align-items: center;
         padding: 0;
+    }
+    .export-data-btn {
+        margin: auto 0 auto auto;
     }
 </style>
